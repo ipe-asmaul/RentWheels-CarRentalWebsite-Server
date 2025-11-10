ee894f6062d9607eeee6373ba6f0b097fe37bfb7
Asmaul <watch.asmaulhossain@gmail.com>
2025-11-10 11:33:24 +0600

    Added Get Queries of the field

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const PORT = process.env.PORT || 5000;
 //middlewire
@@ -41,27 +41,59 @@ async function run() {
       res.send(result);
       console.log(result);
     });
-    app.get('/cars', async(req, res)=>{
-      const db = client.db('carsDB');
-      const carColl = db.collection('cars');
-      const result =await carColl.find({}).toArray()
-      res.send(result)
-    })
+    app.get("/cars", async (req, res) => {
+      const db = client.db("carsDB");
+      const carColl = db.collection("cars");
+      const result = await carColl.find({}).toArray();
+      res.send(result);
+    });
+    app.get("/car/:id", async (req, res) => {
+      const db = client.db("carsDB");
+      const carColl = db.collection("cars");
+      const id = req.params.id;
+      const result = await carColl.findOne({_id: new ObjectId(id)});
+      res.send(result);
+    });
     app.post("/addcar", async (req, res) => {
-      const db = client.db('carsDB');
-      const carColl = db.collection('cars');
+      const db = client.db("carsDB");
+      const carColl = db.collection("cars");
       const car = req.body;
-      const result =await carColl.insertOne(car);
-      res.send(result)
+      const result = await carColl.insertOne(car);
+      res.send(result);
     });
 
-    app.get('/listings/', async(req,res) =>{
-         const query = req.query.email;
-         console.log(query)
-         const db = client.db('carsDB');
-         const carColl = db.collection('cars');
-         const result =await carColl.find({email: query}).toArray();
-         res.send(result)
+    app.get("/listings/", async (req, res) => {
+      const query = req.query.email;
+      console.log(query);
+      const db = client.db("carsDB");
+      const carColl = db.collection("cars");
+      const result = await carColl.find({ email: query }).toArray();
+      res.send(result);
+    });
+    app.patch('/booking-car/:id',async (req,res) =>{
+       const db = client.db("carsDB");
+      const carColl = db.collection("cars");
+      const bookColl = db.collection('bookings');
+      const id = req.params.id;
+      const bookingQuery = {_id: new ObjectId(id)}
+      const alreadyBooked = await bookColl.findOne(bookingQuery);
+      if (alreadyBooked) {
+        return;
+      }
+      const booking = await bookColl.insertOne(req.body)
+      const result = await carColl.updateOne({_id: new ObjectId(id)}, {$set:{isBooked: true}})
+      res.send(booking)
+    })
+
+    app.get('/mybookedcars', async(req,res)=>{
+      const email = req.query.email;
+      const db = client.db("carsDB");
+      const carColl = db.collection("cars");
+      const bookColl = db.collection('bookings');
+      const bookedCars = await bookColl.find({bookingHolder: email}).toArray();
+      const carIds = bookedCars.map(car => new ObjectId(car.carId))
+      const result = await carColl.find({_id: {$in: carIds}}).toArray()
+      res.send(result)
     })
     //testing
     await client.db("admin").command({ ping: 1 });
